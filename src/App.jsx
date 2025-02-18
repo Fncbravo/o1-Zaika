e777d8c1e17f581356aa5c561dac0cc1a99ecef8
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-18 22:45:14 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -25,30 +25,35 @@ const Header = () => {
       <div className="logo-container">
         <img 
            className='logo'
-           src="https://preview.redd.it/how-would-i-take-a-logo-like-a-fast-food-logo-remove-the-v0-vp4l25hezqfd1.jpeg?width=640&crop=smart&auto=webp&s=5cb3aad43002d9455f7c0f4595304e564045036e" alt="Logo" />
+           src="https://preview.redd.it/how-would-i-take-a-logo-like-a-fast-food-logo-remove-the-v0-vp4l25hezqfd1.jpeg?width=640&crop=smart&auto=webp&s=5cb3aad43002d9455f7c0f4595304e564045036e" 
+           alt="Logo"
+         />
       </div>
       <div className="nav-items">
         <ul>
         <li>Home</li>
         <li>About</li>
         <li>Contact Us</li>
-        <li>Cart</li></ul>
+        <li>Cart</li>
+        </ul>
       </div>
     </div>
-  )
+  );
 };
 
-const RestaurantCard = (props) => {
+const RestaurantCard = ({ resName, cuisine, rating, time }) => {
   return (
     <div className="res-card">
       <img 
         className='res-logo'
         alt="res-logo"
-        src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A" alt="Restaurant" />
-     <h3>{props.resName}</h3>
-     <h4>{props.cuisine}</h4>
-     <h4>4.5 stars</h4>
-     <h4>38 minutes</h4>
+        src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A" 
+        alt="Restaurant" 
+        />
+     <h3>{resName}</h3>
+     <h4>{cuisine}</h4>
+     <h4>{rating}</h4>
+     <h4>{time}</h4>
     </div> 
 
   )
@@ -60,13 +65,13 @@ const resObj = {
     type: "F",
     name: "KFC",
     cuisines: "Burger,Fries",
-    avgRating: 3.8
+    avgRating: 3.8,
+    deliveryTime: 30
   }
-}
+};
 
-const resData = resObj;
 
-const Body = (resData) => {
+const Body = () => {
   return (
     <div className="body">
       <div className="search">
@@ -74,14 +79,14 @@ const Body = (resData) => {
       </div>
       <div className="res-container">
         <RestaurantCard 
-        resName= {resData.data.name}
-        cuisine= {resData.data.cuisines}
-        Rating = {resData.data.avgRating}
-        
+        resName= {resObj.data.name}
+        cuisine= {resObj.data.cuisines}
+        rating = {resObj.data.avgRating}
+        time = {resObj.data.deliveryTime}
         />
-        <RestaurantCard resName= "KFC" cuisine="Chicken Burger"/>
-        <RestaurantCard resName= "Dominos" cuisine="Margherita, paneer"/>
-        <RestaurantCard resName= "PizzaHut" cuisine="Fries, latte"/>
+        <RestaurantCard resName= "KFC" cuisine="Burger, Fries" rating={4.2} time={25}/>
+        <RestaurantCard resName= "Domino's" cuisine="Pizza,Pasta" rating={4.1} time={35}/>
+        <RestaurantCard resName= "PizzaHut" cuisine="Pizza, Wings" rating={4.0} time={40}/>
     
       </div>
     </div>
