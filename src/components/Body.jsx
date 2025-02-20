b89abb178eb0b0a59fcdf397d4a36b2f9c908b45
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-21 01:15:22 +0530

    button added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,14 +4,15 @@ import resList from '../utils/mockData'
   export const Body = () => {
     return (
       <div className="body">
-        <div className="search">
-          <input type="text" placeholder="Search" />
+        <div className="filter">
+          <button className='filter-btn' onClick={()=> {
+            console.log("button clicked")
+          }}>Top rated Restaurants</button>
         </div>
         <div className="res-container">
           {resList.map((restaurant) => (
               <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>
-          ))};
-  
+          ))}
         </div>
       </div>
     )
