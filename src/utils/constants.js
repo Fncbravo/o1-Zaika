aa1ca9d8cf4d74b11e38b2930e676d4ecc6b3e6d
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-20 23:10:31 +0530

    update

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const CDN_URL = 
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = 
+"https://preview.redd.it/how-would-i-take-a-logo-like-a-fast-food-logo-remove-the-v0-vp4l25hezqfd1.jpeg?width=640&crop=smart&auto=webp&s=5cb3aad43002d9455f7c0f4595304e564045036e";
+
