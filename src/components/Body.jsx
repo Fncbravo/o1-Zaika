bdb49f219f732fb398b3046ca5aa6c607787768d
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-21 20:54:16 +0530

    updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,15 @@
 import { RestaurantCard } from './RestaurantCard'
 import resList from '../utils/mockData'
+import { useState } from 'react';
 
 
   export const Body = () => {
-  let listOfRestaurants = [
+
+    // State variable - super powerful variable
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+    // normal js variable
+  let listOfRestaurantsJS = [
     {
       info: {
         id: "25620",
@@ -22,8 +28,18 @@ import resList from '../utils/mockData'
           cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
           costForTwo: 4000,
           deliveryTime: 36,
-          avgRating: "1.2",
-        }}
+          avgRating: "4.5",
+        }},
+        {
+          info: {
+            id: "25622",
+            name: "MCD",
+            cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/fc1726b6-290d-463d-9baa-d1c34c0542fc_25620.JPG",
+            cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+            costForTwo: 4000,
+            deliveryTime: 36,
+            avgRating: "4.1",
+          }}
 
 ];
 
@@ -31,11 +47,11 @@ import resList from '../utils/mockData'
       <div className="body">
         <div className="filter">
           <button className='filter-btn' onClick={()=> {
-            // filter logic here
-             listOfRestaurants = listOfRestaurants.filter(
+              
+             const filteredList = listOfRestaurants.filter(
               (res)=> res.info.avgRating > 4 
-            );
-            console.log(listOfRestaurants)
+            ); 
+            setListOfRestaurants(filteredList);
           }}>Top rated Restaurants</button>
         </div>
         <div className="res-container">
