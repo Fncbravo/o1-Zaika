3bd863e3e05374f6de1e783b4388b54c112b71e8
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-21 23:48:44 +0530

    useEffect added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
 import { RestaurantCard } from './RestaurantCard'
 import resList from '../utils/mockData'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
   export const Body = () => {
@@ -8,40 +8,9 @@ import { useState } from 'react';
     // State variable - super powerful variable
     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
-    // normal js variable
-  let listOfRestaurantsJS = [
-    {
-      info: {
-        id: "25620",
-        name: "KFC",
-        cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/fc1726b6-290d-463d-9baa-d1c34c0542fc_25620.JPG",
-        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-        costForTwo: 4000,
-        deliveryTime: 36,
-        avgRating: "3.8",
-      }},
-      {
-        info: {
-          id: "25621",
-          name: "Dominos",
-          cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/fc1726b6-290d-463d-9baa-d1c34c0542fc_25620.JPG",
-          cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-          costForTwo: 4000,
-          deliveryTime: 36,
-          avgRating: "4.5",
-        }},
-        {
-          info: {
-            id: "25622",
-            name: "MCD",
-            cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/fc1726b6-290d-463d-9baa-d1c34c0542fc_25620.JPG",
-            cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-            costForTwo: 4000,
-            deliveryTime: 36,
-            avgRating: "4.1",
-          }}
-
-];
+   useEffect(()=> {
+    console.log("useEffect called")
+   }, []);
 
     return (
       <div className="body">
