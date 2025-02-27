796ac8716ccefe7ae0289dbcd7a08e734c286234
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-28 00:00:42 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import About from './components/About.jsx'
 import Contact from './components/Contact.jsx'
 import Error from './components/Error.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Body } from './components/Body.jsx'
 
 
 
@@ -12,16 +13,23 @@ const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+      path: '/',
+      element: <Body />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      }
+    ],
     errorElement: <Error />
   },
-  {
-    path: '/about',
-    element: <About />
-  },
-  {
-    path: '/contact',
-    element: <Contact />
-  }
+  
 ])
 
 createRoot(document.getElementById('root')).render(
