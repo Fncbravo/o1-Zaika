fd92fe9e1c8d50fd632b1d0f2b9cb7d5f19f696b
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-02-28 00:59:45 +0530

    added routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 
 export const Header = () => {
@@ -21,9 +22,12 @@ export const Header = () => {
         </div>
         <div className="nav-items">
           <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link></li>
+          <li>
+            <Link to="/about">About</Link></li>
+          <li>
+           <Link to="/contact"> Contact Us</Link></li>
           <li>Cart</li>
           <button 
              className="login" 
