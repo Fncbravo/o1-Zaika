99ca5feb81084712faac2d67dd1cf9df6167443b
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-03 22:11:01 +0530

    updated restaurantCard routing

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Contact from './components/Contact.jsx'
 import Error from './components/Error.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Body } from './components/Body.jsx'
+import RestaurantMenu from './components/RestaurantMenu.jsx'
 
 
 
@@ -17,7 +18,7 @@ const appRouter = createBrowserRouter([
       {
       path: '/',
       element: <Body />
-      },
+      },  
       {
         path: '/about',
         element: <About />
@@ -25,6 +26,10 @@ const appRouter = createBrowserRouter([
       {
         path: '/contact',
         element: <Contact />
+      },
+      {
+        path: '/restaurants/:resId',
+        element: <RestaurantMenu />
       }
     ],
     errorElement: <Error />
