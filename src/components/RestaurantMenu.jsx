8517cd403fa582fbe0e82679b33d07356c3478ee
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-03 22:54:47 +0530

    updated

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -24,16 +24,27 @@ if (resInfo === null) return <Shimmer/ >;
   const {name, cuisines, costForTwoMessage} = 
   resInfo?.cards[2]?.card?.card?.info;
 
+  const {itemCards} = 
+  resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  console.log(itemCards)
+
   return (
     <div className="menu">
         <h1>{name}</h1>
-        <h3>{cuisines.join(", ")}</h3>
-        <h3>{costForTwoMessage}</h3>
-        
+        <p>
+          {cuisines.join(", ")} - {costForTwoMessage}
+        </p>
+        <h2>Menu</h2>
         <ul>
-          <li>Biryani</li>
-          <li>Burgers</li>
-          <li>Diet coke</li>
+          {itemCards.map((item) => (
+            <li>{item.card.info.name} - {"Rs "}
+            {item.card.info.price / 100}</li>
+            ))}
+          <li>{itemCards[0].card.info.name}</li>
+          <li>{itemCards[1].card.info.name}</li>
+          <li>{itemCards[2].card.info.name}</li>
+          <li>{itemCards[3].card.info.name}</li>
         </ul>
     </div>
   )
