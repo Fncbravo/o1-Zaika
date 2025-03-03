64aed610de3062dd912477e6b614cf97b9ef19bc
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-03 22:12:07 +0530

    updated restaurantCard component

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,12 +1,42 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
+  
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, [])
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=671928&catalog_qa=undefined&submitAction=ENTER"
+     );
+    const json = await data.json();
+
+    console.log(json);
+    setResInfo(json.data);
+  }
+
+if (resInfo === null) return <Shimmer/ >; 
+
+  const {name, cuisines, costForTwoMessage} = 
+  resInfo?.cards[2]?.card?.card?.info;
+
   return (
     <div className="menu">
-        <h1>Name of the Restaurant</h1>
-        <h2>Menu</h2>
-        <h3>hellow woorld </h3>
+        <h1>{name}</h1>
+        <h3>{cuisines.join(", ")}</h3>
+        <h3>{costForTwoMessage}</h3>
+        
+        <ul>
+          <li>Biryani</li>
+          <li>Burgers</li>
+          <li>Diet coke</li>
+        </ul>
     </div>
   )
 }
 
-export default RestaurantMenu
+export default RestaurantMenu;
