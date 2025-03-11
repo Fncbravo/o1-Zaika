a0730583339239dc5eea4ce76bc93cacfd451d2c
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-11 22:44:22 +0530

    updated

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,9 +3,11 @@ import { useState } from "react"
 const User = ({ name }) => {
 
   const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
         <h1>Count = {count}</h1>
+        <h1>Coun2t = {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: Mumbai</h3>
         <h3>Contact: @sujeetmarch18</h3>
