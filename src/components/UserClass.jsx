1e33a0d157efbd3b04254dea70b07a3b240f7034
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-22 19:40:27 +0530

    updated

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,37 +6,41 @@ class UserClass extends React.Component {
         super(props);
 
         this.state = {
-            count: 0,
-            count2: 2,
+            userInfo: {
+                name: "Dummy",
+                location: "default", 
+            }
         };
-        console.log(this.props.name + "Child Constructor")
+        // console.log(this.props.name + "Child Constructor")
     }
 
-    componentDidMount() {
-        console.log(this.props.name + "Child componentDidMount");
-
+    async componentDidMount() {
+        // console.log(this.props.name + "Child componentDidMount");
         // Api call
+
+        const data = await fetch("https://api.github.com/users/fncbravo");
+        const json = await data.json();
+
+        this.setState({
+            userInfo: json,
+        });
+
+        console.log(json);
     }
  
     render() {
-        const { name, location } = this.props;
-        const { count } = this.state;
 
-        console.log(this.props.name + "Child Render")
+        // console.log(this.props.name + "Child Render")
+
+        const { name, location, avatar_url } = this.state.userInfo;
 
     return (
         <div className="user-card">
-            <h1>Count: {count}</h1>
-            <button onClick={() => {
-                // NEVER UPDATE STATE VAIRABLES DIRECTLY
-                this.setState({
-                    count: this.state.count + 1,
-                    count2: this.state.count2 + 1,
-                })
-            }}>Count increase</button>
+            <img src={avatar_url} alt="user" />
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h3>Contact: @sujeetmarch18</h3>
+            
         </div>
     )
 
