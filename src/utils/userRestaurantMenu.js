db6565a1ac83e5322761faaa69bbe20931259ccf
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2025-03-29 23:50:30 +0530

    custom hook

---
+++ src/utils/userRestaurantMenu.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+const userRestaurantMenu = (resId) => {
+    // fetch data
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    return resInfo;
+}
+
+export default userRestaurantMenu;
